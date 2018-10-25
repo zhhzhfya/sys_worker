fad605a786a4a9ef5742fd408c70ecab9f83f33d
zhenya.zhang <zhhzhfya@163.com>
2018-10-25 11:06:35 +0800

    add files

---
+++ sys_worker/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace sys_worker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_log = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_proc_path = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_proc_name = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(866, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "退出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_log
+            // 
+            this.txt_log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_log.Location = new System.Drawing.Point(12, 113);
+            this.txt_log.Multiline = true;
+            this.txt_log.Name = "txt_log";
+            this.txt_log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_log.Size = new System.Drawing.Size(950, 307);
+            this.txt_log.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "发送邮箱";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(99, 11);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(484, 28);
+            this.txt_email.TabIndex = 3;
+            this.txt_email.Text = "37729782@qq.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "捕捉目标";
+            // 
+            // txt_proc_path
+            // 
+            this.txt_proc_path.Location = new System.Drawing.Point(99, 45);
+            this.txt_proc_path.Name = "txt_proc_path";
+            this.txt_proc_path.Size = new System.Drawing.Size(484, 28);
+            this.txt_proc_path.TabIndex = 5;
+            this.txt_proc_path.Text = "C:\\Program Files (x86)\\TeamViewer\\TeamViewer.exe";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "目标名称";
+            // 
+            // txt_proc_name
+            // 
+            this.txt_proc_name.Location = new System.Drawing.Point(99, 79);
+            this.txt_proc_name.Name = "txt_proc_name";
+            this.txt_proc_name.Size = new System.Drawing.Size(272, 28);
+            this.txt_proc_name.TabIndex = 7;
+            this.txt_proc_name.Text = "TeamViewer 12";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(589, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "发送周期";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(675, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 28);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "小时",
+            "分钟",
+            "秒"});
+            this.comboBox1.Location = new System.Drawing.Point(802, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(65, 26);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(866, 74);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 35);
+            this.btnStart.TabIndex = 11;
+            this.btnStart.Text = "启动";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(764, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 35);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "手动发送";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(630, 89);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 473);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_proc_name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_proc_path);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_log);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "抓屏发送邮件";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_log;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_proc_path;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_proc_name;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ sys_worker/Form1.cs
@@ -0,0 +1,223 @@
+﻿using sys_worker.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace sys_worker
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<String, String> unit = new Dictionary<string, string>();
+        public Form1()
+        {
+            unit.Add("小时", "86400000");
+            unit.Add("分钟", "3600");
+            unit.Add("秒", "1000");
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.txt_proc_path.Text = Settings.Default.proc_path;
+            this.txt_proc_name.Text = Settings.Default.proc_name;
+            this.txt_email.Text = Settings.Default.email;
+            this.numericUpDown1.Value = Settings.Default.period;
+            this.comboBox1.SelectedIndex = Settings.Default.unit;
+
+            String key = this.comboBox1.Items[this.comboBox1.SelectedIndex].ToString();
+            this.timer1.Interval = Convert.ToInt32(unit[key]) * (int)this.numericUpDown1.Value;
+            
+            this.btnStart.PerformClick();//.Click(sender, e);
+        }
+
+        [DllImport("user32.dll ")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        //根据任务栏应用程序显示的名称找相应窗口的句柄
+        [DllImport("User32.dll", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        private const int SW_RESTORE = 9;
+        private void OpenSerialPortUtility(object sender, EventArgs e)
+        {
+            //第一种方法
+            //查找状态中的窗口线程句柄来查看目标程序是否在运行运行则前置否则打开
+            IntPtr findPtr = FindWindow(this.txt_proc_name.Text, null);
+            if (findPtr.ToInt32() != 0)
+            {
+                ShowWindow(findPtr, SW_RESTORE); //将窗口还原，如果不用此方法，缩小的窗口不能激活
+                SetForegroundWindow(findPtr);//将指定的窗口选中(激活)
+            }
+            else
+            {
+                try
+                {
+                    Process proc = System.Diagnostics.Process.Start(this.txt_proc_path.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            addLog("任务启动");
+            /// 1 bring the process to front
+            OpenSerialPortUtility(sender, e);
+            Thread.Sleep(1000);
+            /// 2 capture the screen
+            captureScreen();
+            /// 3 send email
+            sendEmail();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenSerialPortUtility(sender, e);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            captureScreen();
+        }
+
+        private void captureScreen()
+        {
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+                bitmap.Save("test.jpg", ImageFormat.Jpeg);
+            }
+            addLog("截屏完成");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sendEmail();
+        }
+
+        public void sendEmail()
+        {
+            //实例化一个发送邮件类。
+            MailMessage mailMessage = new MailMessage();
+            //发件人邮箱地址，方法重载不同，可以根据需求自行选择。
+            mailMessage.From = new MailAddress("37729782@qq.com");
+            //收件人邮箱地址。
+            mailMessage.To.Add(new MailAddress(this.txt_email.Text));
+            //邮件标题。
+            mailMessage.Subject = "发送邮件测试";
+            //邮件内容。
+            mailMessage.Body = "这是我给你发送的第一份邮件哦！";
+            
+            mailMessage.Attachments.Add(new System.Net.Mail.Attachment(System.Environment.CurrentDirectory + "\\test.jpg"));
+            //实例化一个SmtpClient类。
+            SmtpClient client = new SmtpClient();
+            //在这里我使用的是qq邮箱，所以是smtp.qq.com，如果你使用的是126邮箱，那么就是smtp.126.com。
+            client.Host = "smtp.qq.com";
+            //使用安全加密连接。
+            client.EnableSsl = true;
+            //不和请求一块发送。
+            client.UseDefaultCredentials = false;
+            //验证发件人身份(发件人的邮箱，邮箱里的生成授权码);
+            client.Credentials = new NetworkCredential("37729782@qq.com", "dzyuqklchtvqbhaa");
+            //发送
+            client.Send(mailMessage);
+            mailMessage.Dispose();
+            addLog("邮件发送成功");
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            addLog((int)this.numericUpDown1.Value);
+            this.timer1.Enabled = false;
+            String key = this.comboBox1.Items[this.comboBox1.SelectedIndex].ToString();
+            addLog(Convert.ToInt32(unit[key]));
+            if (Convert.ToInt32(unit[key]) * (decimal)this.numericUpDown1.Value < 10)
+            {
+                addLog("设定错误，必须大于10");
+                return;
+            }
+            this.timer1.Interval = (int)(Convert.ToInt32(unit[key]) * (decimal)this.numericUpDown1.Value);
+            this.timer1.Enabled = true;
+            addLog("任务重新设定，周期" + this.numericUpDown1.Value + key);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (!this.timer1.Enabled)
+            {
+                this.timer1.Enabled = true;
+                this.btnStart.Text = "停止";
+                addLog("启动成功");
+            }
+            else {
+                this.timer1.Enabled = false;
+                this.btnStart.Text = "启动";
+                addLog("停止工作");
+            }
+        }
+
+        private void addLog(object log)
+        {
+            this.txt_log.AppendText(DateTime.Now.ToLongDateString() + " "+log+"\r\n");
+            this.txt_log.Focus();//获取焦点
+            this.txt_log.Select(this.txt_log.TextLength, 0);//光标定位到文本最后
+            this.txt_log.ScrollToCaret();//滚动到光标处
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+            /// 1 bring the process to front
+            OpenSerialPortUtility(sender, e);
+            Thread.Sleep(1000);
+            /// 2 capture the screen
+            captureScreen();
+            /// 3 send email
+            sendEmail();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Settings.Default.email = this.txt_email.Text;
+            Settings.Default.proc_path = this.txt_proc_path.Text;
+            Settings.Default.proc_name = this.txt_proc_name.Text;
+            Settings.Default.period = (int)this.numericUpDown1.Value;
+            Settings.Default.unit = this.comboBox1.SelectedIndex;
+
+            Settings.Default.Save();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show((int.MaxValue / 86400000) + "");
+        }
+    }
+
+    
+}
